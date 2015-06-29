4cf86b2bfa24c2613ff87798a67e890f82225195
Shakir Mengrani <shakir.mengrani@gmail.com>
2015-06-29 23:46:46 +0500

    change

--- Angular/app.js
+++ Angular/app.js
@@ -6,6 +6,7 @@ if (typeof __decorate !== "function") __decorate = function (decorators, target,
         case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
     }
 };
+};
 /// <reference path="typings/angular2/angular2.d.ts" />
 var angular2_1 = require('angular2/angular2');
 var dataFactory = (function () {
