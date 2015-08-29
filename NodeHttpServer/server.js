1305e63627a4ceb9f98a2976a053c6b8b348b0f3
Shakir Mengrani <shakir.mengrani@gmail.com>
2015-08-30 00:43:39 +0500

    Add api

--- NodeHttpServer/server.js
+++ NodeHttpServer/server.js
@@ -4,7 +4,8 @@ var fs = require("fs");
 var status = 200;
 var port = 8080;
 var fileMap = {
-    default: 'index.html'
+    default: 'index.html',
+    userList: 'users/userList.html'
 };
 // Response data from mapped file
 function getFileForUrl(res, resFile) {
@@ -16,11 +17,29 @@ function getFileForUrl(res, resFile) {
         res.end();
     });
 }
+// Response special content-typed data
+function getCustomOutput(res, data) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(data);
+    res.end();
+}
 // Set File as per URI / Route
 function route(URI, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     switch (URI) {
         case "/":
             getFileForUrl(res, fileMap.default);
+            break;
+        case '/getUsers':
+            var data = {
+                users: [
+                    { name: "shakir", Email: "shakir.mengrani@gmail.com" }
+                ]
+            };
+            getCustomOutput(res, JSON.stringify(data, null, 4));
+            break;
+        case "/userlist":
+            getFileForUrl(res, fileMap.userList);
             break;
         default:
             res.end("<h1>404 - page not found</h1>");
@@ -29,7 +48,6 @@ function route(URI, res) {
 }
 // Http Server Handler
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
     route(req.url, res);
 }).listen(port);
 console.log("Your Server starts on localhost:" + port + "!");
--- NodeHttpServer/server.ts
+++ NodeHttpServer/server.ts
@@ -1,38 +1,56 @@
 /// <reference path="typings/node/node.d.ts" />
 import http = require('http');
 import fs = require("fs");
-var status:number = 200;
-var port:number = 8080;
+var status: number = 200;
+var port: number = 8080;
 var fileMap = {
-	default:'index.html'
+	default: 'index.html',
+	userList:'users/userList.html'
 }
 
 // Response data from mapped file
-function getFileForUrl(res:http.ServerResponse,resFile:string){
-	fs.readFile(resFile,function(err,data){
-		if (err){
+function getFileForUrl(res: http.ServerResponse, resFile: string) {
+	fs.readFile(resFile, function(err, data) {
+		if (err) {
 			return console.log(err);
 		}
 		res.write(data);
-	 	res.end();
-	});	
+		res.end();
+	});
+}
+// Response special content-typed data
+function getCustomOutput(res: http.ServerResponse, data: string) {
+	res.writeHead(200, { 'Content-Type': 'application/json' });
+	res.write(data)
+	res.end();
 }
 
 // Set File as per URI / Route
-function route(URI:string,res:http.ServerResponse){
-	switch(URI){
+function route(URI: string, res: http.ServerResponse) {
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	switch (URI) {
 		case "/":
-			getFileForUrl(res,fileMap.default);
-		break;
+			getFileForUrl(res, fileMap.default);
+			break;
+		case '/getUsers':
+			var data = {
+				users:[
+					{name:"shakir",Email:"shakir.mengrani@gmail.com"}
+				]
+			};
+			getCustomOutput(res, JSON.stringify(data,null,4));
+			break;
+			case "/userlist":
+				getFileForUrl(res,fileMap.userList);
+			break;
 		default:
-		res.end("<h1>404 - page not found</h1>");
-		break;
+			res.end("<h1>404 - page not found</h1>");
+			break;
 	}
 }
 
 // Http Server Handler
-http.createServer(function(req,res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	route(req.url,res);
+http.createServer(function(req, res) {
+	route(req.url, res);
 }).listen(port);
 console.log("Your Server starts on localhost:" + port + "!");
